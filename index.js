9cfe89f9b380abaf9135ffbdb827fc765494add7
andy-aguilar <aaguil3@gmail.com>
2021-03-08 17:56:04 -0600

    commit tree e854955c4ca06b0569257164271ced398862360f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function mapToNegativize(array){
+    let new_array = []
+    for(let i = 0; i < array.length; i++){
+        new_array.push(array[i] * -1)
+    }
+    return new_array
+}
+
+function mapToNoChange(array){
+    return array
+}
+
+function mapToDouble(array){
+    let new_array = []
+    for(let i = 0; i < array.length; i++){
+        new_array.push(array[i] * 2)
+    }
+    return new_array
+}
+
+function mapToSquare(array){
+    let new_array = []
+    for(let i = 0; i < array.length; i++){
+        new_array.push(array[i]**2)
+    }
+    return new_array
+}
+
+function reduceToTotal(array, starting=0){
+    let total = starting
+    for(let i=0; i<array.length; i++){
+        total = total + array[i]
+    }
+    return total
+}
